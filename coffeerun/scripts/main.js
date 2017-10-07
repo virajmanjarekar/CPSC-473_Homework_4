8e5ba7cd32525d305ed97d335e972e4ea3fff119
virajmanjrekar@gmail.com <virajman@csu.fullerton.edu>
2017-10-07 13:04:34 -0700

    homework 4 draft 1

---
+++ coffeerun/scripts/main.js
@@ -0,0 +1,31 @@
+(function(window) {
+'use strict';
+var FORM_SELECTOR = '[data-coffee-order="form"]';
+var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+ var SERVER_URL = 'http://localhost:2403/coffeorders/';
+var App = window.App;
+var Truck = App.Truck;
+var DataStore = App.DataStore;
+ var RemoteDataStore = App.RemoteDataStore;
+var FormHandler = App.FormHandler;
+var Validation = App.Validation;
+var CheckList = App.CheckList;
+var remoteDS = new RemoteDataStore(SERVER_URL);
+var webshim = window.webshim;
+var myTruck = new Truck('ncc-1701', remoteDS);
+window.myTruck = myTruck;
+var checkList = new CheckList(CHECKLIST_SELECTOR);
+checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+var formHandler = new FormHandler(FORM_SELECTOR);
+formHandler.addSubmitHandler(function(data) {
+  myTruck.createOrder.call(myTruck, data);
+  checkList.addRow.call(checkList, data);
+});
+formHandler.addInputHandler(Validation.isCompanyEmail);
+
+/*webshim.polyfill('forms forms-ext');
+webshim.setOptions('forms', {
+  addValidators: true,
+  lazyCustomMessages: true
+});*/
+})(window);
